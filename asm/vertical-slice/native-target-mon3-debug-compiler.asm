6bbeb9187bc867a836033f8d603aaaae359374aa
John Hardy <jh@lagado.com>
2026-08-25 02:17:52 +1000

    Fix native service image placement

--- asm/vertical-slice/native-target-mon3-debug-compiler.asm
+++ asm/vertical-slice/native-target-mon3-debug-compiler.asm
@@ -5,11 +5,11 @@ NativeStreamingSource .equ 1
 Mon3HostTransport .equ 1
             .include "mon3-target-memory-map.asmi"
             .include "nucleus-runtime-identity.asmi"
-            .org CompilerCoreBase
-            .include "flat-target-compiler-image.asmi"
-            .include "native-host-vector.asmi"
             .org $4400
 NativeSystemServicesBase:
             .include "platform-services-abi.asmi"
             .include "native-source-plan-provider.asm"
             .include "native-system-services.asm"
+            .org CompilerCoreBase
+            .include "flat-target-compiler-image.asmi"
+            .include "native-host-vector.asmi"
